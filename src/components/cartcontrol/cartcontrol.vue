dc4285d3c891f9b48b1ccc3258f7a6c0d5d8ecf0
solo_pc <cj@cachecats.com>
2018-06-02 12:28:04 +0800

    update

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="wrapper">
-    <div class="cart-decrease icon-remove_circle_outline" v-if="food.count>0" @click="decreaseCount"></div>
+    <div class="cart-decrease icon-remove_circle_outline" v-if="food.count>0" @click.stop.prevent="decreaseCount"></div>
     <div class="cart-count" v-if="food.count>0">{{food.count}}</div>
-    <div class="cart-add icon-add_circle" @click="addCount"></div>
+    <div class="cart-add icon-add_circle" @click.stop.prevent="addCount"></div>
   </div>
 
 </template>
